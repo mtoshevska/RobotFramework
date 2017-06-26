593be38dd05085f0109859ac478d3d0aca5b2295
Martina <martina.tosevska.95@gmail.com>
2017-06-26 19:30:25 +0200

    Add Converter.py

---
+++ Resources/Converter.py
@@ -0,0 +1,21 @@
+class Converter:
+    eur=0
+
+    def __init__(self,eur):
+        self.eur=eur
+
+    def __str__(self):
+        return str(self.eur)
+
+    def eur_to_usd(self):
+        return self.eur*1.12
+
+    def eur_to_mkd(self):
+        return self.eur*61.69
+
+    def eur_to_chf(self):
+        return self.eur*1.09
+
+    def eur_to_dem(self):
+        return self.eur*1.92
+    
